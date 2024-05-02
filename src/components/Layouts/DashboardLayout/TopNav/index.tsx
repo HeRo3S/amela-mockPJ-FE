87f346f7cf40cd3a2ecb849c4320e782bc382dee
HeRo3S <hungid6a1@gmail.com>
2024-05-02 14:25:53 +0700

    copy all MUI breakpoints to scss glob vars

--- src/components/Layouts/DashboardLayout/TopNav/index.tsx
+++ src/components/Layouts/DashboardLayout/TopNav/index.tsx
@@ -1,21 +1,10 @@
 import Bars3Icon from "@heroicons/react/24/solid/Bars3Icon";
-import {
-  Avatar,
-  Box,
-  IconButton,
-  Stack,
-  SvgIcon,
-  useMediaQuery,
-} from "@mui/material";
-import { alpha } from "@mui/material/styles";
+import { Avatar, Box, IconButton, Stack, SvgIcon } from "@mui/material";
 import { usePopover } from "utils/hooks/usePopover";
 import AccountPopover from "../AccountPopover";
 import { useSelector } from "react-redux";
 import { useAppSelector } from "utils/hooks/reduxToolkit";
 import styles from "./style.module.scss";
-
-const SIDE_NAV_WIDTH = 280;
-const TOP_NAV_HEIGHT = 64;
 
 interface IProps {
   onNavOpen: () => void;
@@ -23,62 +12,30 @@ interface IProps {
 function TopNav(props: IProps) {
   const { onNavOpen } = props;
   const { userInfo } = useAppSelector((state) => state.auth);
-  const lgUp = useMediaQuery((theme) => theme.breakpoints.up("lg"));
   const accountPopover = usePopover();
 
   return (
     <>
-      <Box
-        component="header"
-        sx={{
-          backdropFilter: "blur(6px)",
-          backgroundColor: (theme) =>
-            alpha(theme.palette.background.default, 0.8),
-          position: "sticky",
-          left: {
-            lg: `${SIDE_NAV_WIDTH}px`,
-          },
-          top: 0,
-          width: {
-            lg: `calc(100% - ${SIDE_NAV_WIDTH}px)`,
-          },
-          zIndex: (theme) => theme.zIndex.appBar,
-        }}
-      >
-        <Stack
-          alignItems="center"
-          direction="row"
-          justifyContent="space-between"
-          spacing={2}
-          sx={{
-            minHeight: TOP_NAV_HEIGHT,
-            px: 2,
-          }}
-        >
+      <Box component="header" className={styles.headerWrapper}>
+        <div className={styles.buttonsStack}>
           <Stack alignItems="center" direction="row" spacing={2}>
-            {!lgUp && (
-              <IconButton onClick={onNavOpen}>
-                <SvgIcon fontSize="small">
-                  <Bars3Icon />
-                </SvgIcon>
-              </IconButton>
-            )}
+            <IconButton className={styles.menuButton} onClick={onNavOpen}>
+              <SvgIcon fontSize="small">
+                <Bars3Icon />
+              </SvgIcon>
+            </IconButton>
           </Stack>
           <Stack alignItems="center" direction="row" spacing={2}>
             <Avatar
+              className={styles.styledAvt}
               onClick={accountPopover.handleOpen}
               ref={accountPopover.anchorRef}
-              sx={{
-                cursor: "pointer",
-                height: 40,
-                width: 40,
-              }}
-              src={`${import.meta.env.VITE_APP_BE_SEVER_URL}/img/users/${
+              src={`${import.meta.env.VITE_APP_BE_SERVER_URL}/img/users/${
                 userInfo?.photo
               }`}
             />
           </Stack>
-        </Stack>
+        </div>
       </Box>
       <AccountPopover
         anchorEl={accountPopover.anchorRef.current}
